92f4ae51288ad52bd75856f4252b1ff097d83605
TIVMOF <tomyivnov06@gmail.com>
2024-11-12 13:22:29 +0200

    feat: Finishing touches

--- cocosolis-home-page/scripts.js
+++ cocosolis-home-page/scripts.js
@@ -1,29 +1,17 @@
-var hasScrolledOnce = false;
-window.addEventListener('scroll', function () {
-    const lowerHeader = document.getElementById('lower_header');
-    const wholeHeader = document.getElementById('main_header');
-    const logoImage = document.getElementById('main-logo');
-    const scrollPosition = window.scrollY || 0;
+window.addEventListener("scroll", function() {
+    const headerBottomRow = document.querySelector(".header-bottom-row");
+    const mainHeaderContainer = document.querySelector(".main-header-container");
+    const mainLogo = document.querySelector("#main-logo");
 
-    if (scrollPosition > 100 && !hasScrolledOnce) {
-        hasScrolledOnce = true;
-        console.log("has scrolled once true");
+    if (window.scrollY > 100) {
+        headerBottomRow.classList.add("show-header-bottom-row");
+        mainHeaderContainer.classList.add("shrink-header");
+        mainLogo.classList.add("shrink-logo");
     }
-
-    if (hasScrolledOnce && scrollPosition == 0) {
-        lowerHeader.style.display = "none";
-        wholeHeader.style.display = "flex";
-        logoImage.style.width = "204px";
-        logoImage.style.padding = "16px 0px";
-        console.log("lower none");
-    } 
-
-    if (scrollPosition > 80) {
-        wholeHeader.style.position = "sticky";
-        lowerHeader.style.display = "flex";
-        wholeHeader.style.top = "0";
-        logoImage.style.width = "90px";
-        logoImage.style.padding = "8px 0px";
-        console.log("sticky");
+    
+    else {
+        headerBottomRow.classList.remove("show-header-bottom-row");
+        mainHeaderContainer.classList.remove("shrink-header");
+        mainLogo.classList.remove("shrink-logo");
     }
 });
